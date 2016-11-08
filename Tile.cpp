07aacc6ffd7d30b2d5e6bdf0d17d2d89acf7beec
Austin Lindenberg <austindlindenber@ufl.edu>
2016-11-08 10:58:16 -0500

    getOpenFaces function in Face

--- Tile.cpp
+++ Tile.cpp
@@ -2,7 +2,7 @@
 
 Tile()
 {
-	
+
 }
 
 ~Tile()
@@ -15,8 +15,19 @@ string to_string()
 	
 }
 
-void rotate(int rotations)
+// Rotate a tile to the right by 90 degrees. (Ex: left face would now be top face, top face would be right, etc...)
+void rotate(Tile tile)
 {
+	Face upFace = this.up;
+	Face downFace = this.down;
+	Face leftface = this.left;
+	Face rightFace = this.right;
+
+	this.up = leftFace;
+	this.right = upFace;
+	this.down = rightFace;
+	this.left = downFace;
+	
 	
 }
 
@@ -24,6 +35,22 @@ bool hasOpenFace()
 {
 	if(!up.connected() || !down.connected() || !left.connected() || !right.conncted()) return true;
 	else return false;
+}
+
+// Method to obtain all the open faces of a tile. Check each face, if not connected then push to vector
+std::vector<Tile> getOpenFaces() 
+{
+	std::vector<Tile> openFaces;
+	if(!up.connected())
+		openFaces.pushBack(up);
+	if(!down.connected())
+		openFaces.pushBack(down);
+	if(!left.connected())
+		openFaces.pushBack(left);
+	if(!right.connected())
+		openFaces.pushBack(right);
+
+	return openFaces;
 }
 
 bool shield
