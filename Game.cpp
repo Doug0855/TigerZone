44db53720b276d25b60d10d601b2693c652f1823
Doug0855 <doug0855@gmail.com>
2016-11-10 14:17:48 -0500

    Fleshed out Game methods a little

--- Game.cpp
+++ Game.cpp
@@ -1,11 +1,22 @@
 #include "Game.h"
 
-Game::Game()
+Game::Game(Player p1, Player p2, TileStack t, Board b)
 {
-	
+	player1 = p1;
+	player2 = p2;
+	tileStack = t;
+	gameBoard = b;
 }
 
 Game::~Game()
 {
 	
+}
+
+void Game::init() {
+	currentPlayer = 0;
+	tileStack.shuffle(); 
+}
+
+void Game::play() {
 }
--- Game.h
+++ Game.h
@@ -1,13 +1,20 @@
 #pragma once
+#include "Player.h"
+#include "Board.h"
+#include "TileStack.h"
+
 class Game{
 	
 public:
-	Game();
+	Game(Player p1, Player p2, TileStack t, Board b);
 	~Game();
-	
+	void init(); 
+	void play(); 
 private:
-		
-	
+	Board gameBoard; 
+	Player player1, player2;
+	int currentPlayer; 
+	TileStack tileStack; 
 };
 
 
