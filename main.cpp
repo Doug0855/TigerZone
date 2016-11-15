76710f4308ca2f07d5b54d07e277cdfb45211784
Austin Lindenberg <austindlindenber@ufl.edu>
2016-11-15 14:11:58 -0500

    set up main.cpp to play all the tiles and check validity in gui

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "Block.cpp"
 #include "Face.cpp"
 #include "Meeple.cpp"
+#include "TileStack.cpp"
 
 void printToTextFile(Board gameboard)
 {
@@ -27,47 +28,37 @@ void printToTextFile(Board gameboard)
 int main() {
 	Board gameBoard;	
 
-	Tile tile1(1);	
-	Tile tile2(2);
-	Tile tile3(8);
+	Tile tile1(19);	
+	TileStack tStack;
+	tStack.shuffle();
 
-	tile1.setType('b');
-	tile2.setType('b');
-	tile3.setType('b');
+	gameBoard.place_tile(std::pair<int,int>(72,72), tile1);
 
-	gameBoard.place_tile(std::pair<int,int>(71,72), tile1);
-	// gameBoard.place_tile(std::pair<int,int>(72,72), tile2);
-	// gameBoard.place_tile(std::pair<int,int>(73,72), tile3);
+	for(int i = 0; i < tStack.tiles.size(); i ++) {
+		Tile tile = tStack.tiles[i];
 
-	// Get coordinates of all possible locations where we can place a tile 
-	std::vector<std::pair<int,int> > availableLocations = gameBoard.display_positions(&tile3);
-
-	std::cout<<"# of open places: "<<availableLocations.size()<<std::endl;
-	for(int i = 0; i < availableLocations.size(); i++) {
-		int row = availableLocations[i].first;
-		int col = availableLocations[i].second;
-
-		std::cout<<"Open Location at "<<row<<" "<<col<<std::endl;
+		std::vector<std::pair<int,int> > availableLocations = gameBoard.display_positions(&tile);
+		if(availableLocations.size() > 0) {
+			std::pair<int,int> optimalLocation = gameBoard.getOptimalPlacement(&tile, availableLocations);	
+			gameBoard.place_tile(optimalLocation, tile);	
+		}
 	}
-	if(availableLocations.size() > 0)
-		std::pair<int,int> optimalLocation = gameBoard.getOptimalPlacement(&tile3, availableLocations);
-	gameBoard.place_tile(std::pair<int,int>(0,0), tile3);
 
 	printToTextFile(gameBoard);
 	// int z;
 	// std::cin >> z;
 	
 	// Print out gameboard
-	// for(int i = 0; i < gameBoard.m_board.size(); i++)
-	// {
-	// 	for(int j = 0; j < gameBoard.m_board[0].size(); j++)
-	// 	{
-	// 		if(gameBoard.m_board[i][j].hasInit)
-	// 			std::cout<<'A';
-	// 		else
-	// 			std::cout<<'.';
-	// 	}
-	// 	std::cout<<std::endl;
-	// }
+	for(int i = 0; i < gameBoard.m_board.size(); i++)
+	{
+		for(int j = 0; j < gameBoard.m_board[0].size(); j++)
+		{
+			if(gameBoard.m_board[i][j].hasInit)
+				std::cout<<'A';
+			else
+				std::cout<<'.';
+		}
+		std::cout<<std::endl;
+	}
 };
 
