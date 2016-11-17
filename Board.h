8fd4b9d366e116c944d36ad543e1b7791c8f92a1
cmoler <coltonmoler@gmail.com>
2016-11-17 11:05:59 -0500

    cleaned up header files

--- Board.h
+++ Board.h
@@ -5,17 +5,22 @@
 #include <string>
 
 class Board {
-private:
 public:
-	// Made m_board public to be able to access it in main.cpp
-	std::vector< std::vector<Tile> > m_board;
 	Board();
 	~Board();
-	std::string to_string(void);
+
+	// Displays all possible location the tile can be placed on the board in any rotation
 	std::vector< std::pair<int, int> > display_positions(Tile tile);
-	void place_tile(std::pair<int, int> location, Tile& tile);
+	// helper funtion for display_positions 
 	std::vector< std::pair<int, int> > checkPlacement(Tile tile, int i, int j, std::string location);
+	// gets best placement and rotation for current tile
 	std::pair<int, int> getOptimalPlacement(Tile& tile, std::vector< std::pair<int, int> > availableMoves);
+	// places the tile on the board and connects the faces
+	void place_tile(std::pair<int, int> location, Tile& tile);
+	// Made m_board public to be able to access it in main.cpp
+	std::vector< std::vector<Tile> > m_board;
+
+private:
 
 };
 
--- Face.h
+++ Face.h
@@ -10,11 +10,10 @@ public:
 	Face();
 	~Face();
 	
-	Face* neighborFace;
 	bool faceEquals(Face face);	//determines if two faces are equal
 	bool connected(); ///< returns false if not connected
 	
-//private:
+	Face* neighborFace;
 	Block m_1, m_2, m_3;
 	
 };
